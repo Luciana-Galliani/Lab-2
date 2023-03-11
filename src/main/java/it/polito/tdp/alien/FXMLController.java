f0f924950673173a890042473af97069fef97db5
hp <hp@LAPTOP-P968IMR0.wind3.hub>
2023-03-11 11:55:53 +0100

    v1

--- src/main/java/it/polito/tdp/alien/FXMLController.java
+++ src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -6,18 +6,87 @@ package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import java.util.*; //per la mappa
 
 public class FXMLController {
-
-    @FXML // ResourceBundle that was given to the FXMLLoader
+	//attributi
+	TreeMap<String, String> dizionarioAlieno= new TreeMap<String, String>();
+	boolean checkNumero= false;
+	boolean checkNumero2= false;
+	//
+	
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnTranslate;
+
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private TextArea txtTranslate;
+
+    @FXML
+    void doClear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doTranslate(ActionEvent event) {
+    	String array[];
+    	array= this.txtParola.getText().toLowerCase().split(">");
+    	
+    	//devo restituire la traduzione
+    	if(array.length==1) {
+    		this.txtTranslate.setText(dizionarioAlieno.get(array[0].split("<")[1]));
+    	}
+    	
+    	//devo aggiungere la parola al dizionario
+    	else if(array.length==2) {
+    		
+    		String parolaAliena= array[0].split("<")[1]; //1 perchè alla posizione zero c'è lo spazio
+    		String traduzione= array[1].split("<")[1];
+    		
+    		for(char c: parolaAliena.toCharArray()) {
+				if(Character.isDigit(c))
+					checkNumero=true;
+			}
+    		for(char c: traduzione.toCharArray()) {
+				if(Character.isDigit(c))
+					checkNumero2=true;
+			}
+    		
+    		if(!checkNumero && !checkNumero2) {
+    			dizionarioAlieno.put(parolaAliena, traduzione);
+    			this.txtParola.clear();
+    		}else {
+    			this.txtTranslate.setText("Ammesse solo le lettere alfabetiche");
+    		}
+    		
+    	}else {
+    		this.txtTranslate.setText("Non viene rispettato il pattern");
+    	}
+
+    }
+
+    @FXML
     void initialize() {
+        assert btnClear != null : "fx:id=\"btnClear\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnTranslate != null : "fx:id=\"btnTranslate\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTranslate != null : "fx:id=\"txtTranslate\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 
